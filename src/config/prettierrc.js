9a0b31d12b873320168ea6bee881bc42698b1a0e
William Feng <william.feng@bappo.com>
2020-03-20 11:05:35 +1100

    feat: update prettier options

--- src/config/prettierrc.js
+++ src/config/prettierrc.js
@@ -1,18 +1,14 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSpacing: false,
+  // This will be the default in Prettier 2
+  // https://github.com/prettier/prettier/pull/7430
+  arrowParens: 'always',
+  // This will be the default in Prettier 2
+  // https://github.com/prettier/prettier/pull/7435
   endOfLine: 'lf',
-  htmlWhitespaceSensitivity: 'css',
-  insertPragma: false,
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: 'always',
-  quoteProps: 'as-needed',
-  requirePragma: false,
-  semi: false,
+  // This is not the default but seems to be more popular in the community and
+  // what we use in the bappo platform projects.
   singleQuote: true,
-  tabWidth: 2,
+  // The default in Prettier 2 will be 'es5' but we always use es6+, so setting
+  // it to 'all' for even better diff readability
   trailingComma: 'all',
-  useTabs: false,
-}
+};
